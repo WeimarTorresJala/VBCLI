148badfc2b20ac6c40962278654bfd08ae23a6da
WeimarTorres <alexandertorreshe@gmail.com>
2021-01-22 22:48:48 -0400

    Test Jenkins 2

--- src/main/java/com/example/VBCLI.java
+++ src/main/java/com/example/VBCLI.java
@@ -4,6 +4,7 @@ public class VBCLI {
 
     public static void main(String[] args) {
         VBManager box = new VBManager();
+        // Test Jenkins 2
 
         System.out.println("Virtual Box version: " + box.getVBoxVersion());
     }
